5b1b45ab2d587337610bc39c8953619ee52b4960
Joshua Balbi <Balbi.joshua@gmail.com>
2025-08-27 23:46:09 -0400

    I want to commit all changes

--- lab_1/lab-task-squirrel/lab-task-squirrel/PhotoViewController.swift
+++ lab_1/lab-task-squirrel/lab-task-squirrel/PhotoViewController.swift
@@ -5,4 +5,14 @@
 //  Created by Diego Balbi on 8/27/25.
 //
 
-import Foundation
+import UIKit
+
+class PhotoViewController: UIViewController {
+    @IBOutlet weak var photoView: UIImageView!
+    var task: Task!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        photoView.image =  task.image
+    }
+}
